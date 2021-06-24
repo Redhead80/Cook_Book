97fb659c973ec7097714806f94bd3acf71d4773c
Геннадий <g.utkin80@yandex.ru>
2021-06-24 13:15:09 +0300

    1

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def dict_collector(file_path):
             counter = file_work.readline().strip()
             list_of_ingridient = []
             for i in range(int(counter)):
-                dish_items = dict.fromkeys(['ingredient_name', 'quantity', 'measure']) # - временный словарь с ингридиетом
-                ingridient = file_work.readline().strip().split(' | ') # - вот так перемещаемся по файлу
+                dish_items = dict.fromkeys(['ingredient_name', 'quantity', 'measure'])    # - временный словарь с ингридиетом
+                ingridient = file_work.readline().strip().split(' | ')    # - вот так перемещаемся по файлу
                 for item in ingridient:
                     dish_items['ingredient_name'] = ingridient[0]
                     dish_items['quantity'] = ingridient[1]
@@ -42,23 +42,16 @@ def get_shop_list_by_dishes(dishes, persons=int):
     pprint(menu)
     print()
     shopping_list = {}
-##     pprint(menu.keys())
     try:
         for dish in dishes:
             for item in (menu[dish]):
-                # print(item['ingredient_name'])
-                # print(item['measure'])
-                # print(item['quantity'])
                 items_list = dict([(item['ingredient_name'], {'measure': item['measure'], 'quantity': int(item['quantity'])*persons})])
                 if shopping_list.get(item['ingredient_name']):
-                    # print(f' Такое {items_list} уже есть в списке. Добавил еще')
                     extra_item = (int(shopping_list[item['ingredient_name']]['quantity']) +
                                   int(items_list[item['ingredient_name']]['quantity']))
-                    # print(extra_item)
                     shopping_list[item['ingredient_name']]['quantity'] = extra_item
 
                 else:
-                    # shopping_list[item['ingredient_name']]['quantity'] *= persons
                     shopping_list.update(items_list)
 
         print(f"Для приготовления блюд на {persons} человек  нам необходимо купить:")
